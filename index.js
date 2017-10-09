00dd3826a617794e85fd89f8ebf22339aadfe26c
Richard Hoefer <rhoefer@rhoefer-rmac-2.local>
2017-10-09 11:08:52 -0400

    Fixing up a few things

--- index.js
+++ index.js
@@ -11,9 +11,18 @@ var path = require('path');
 var shell = require('shelljs');
 var cam = require("raspicam");
 var uuid = require('uuid/v1');
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
+global.brightness = 0;
+global.saturation = 0;
+global.fuzz = 0;
 
 app.set('views',  __dirname + '/templates');
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('public'));
 app.use(express.static('pics'));
 app.use(express.static('backgrounds'));
@@ -72,17 +81,67 @@ app.get('/stream/stop', (req, res) => {
 	res.json();
 });
 
+app.get('/config', (req, res) => {
+    stopStreaming();
+	
+	res.render('config', {
+		fuzz: global.fuzz,
+		saturation: global.saturation,
+		brightness: global.brightness
+	});
+});
+
+app.post('/config/test', upload.array(), (req,res) => {
+	console.log("Running test configuration");
+	
+	var params = req.body;
+	
+	var testParams = {
+		fuzz: params.fuzz,
+		saturation: params.saturation,
+		brightness: params.brightness
+	}
+	
+	var file = "test-output.jpg";
+	var dir = __dirname + "/public/";
+	takePic(file);
+	brightenPic(file, testParams.brightness, testParams.saturation);
+	processMask(dir + file, dir +  file, testParams.fuzz);
+	
+	res.sendStatus(200);
+});
+
+app.post('/config/save', upload.array(), (req, res) => {
+	
+	var params = req.body;
+	
+	console.log("Saving settings");
+	
+	brightness= params.brightness;
+	saturation = params.saturation;
+	fuzz = params.fuzz;
+	
+	saveSettings(fuzz, brightness, saturation);
+	res.sendStatus(200);
+})
+	
 
 app.get('/takePic', (req, res) => {
+	
 	var filename = uuid();
 	var extension = ".jpg"
-	var cmd = "wget http://localhost:8080/?action=snapshot -O " + __dirname + "/public/" + filename + extension;
 	
-	shell.exec(cmd, {silent: true}); // This can't be async.. otherwise the streaming stops and there's no image!
+	var file = filename + extension;
+	var dir = __dirname + "/public/";
+	var brightness = global.brightness;
+	var saturation = global.saturation;
+	var fuzz = global.fuzz;
 	
-	// Process the image here
-	var cmd = 'mogrify -modulate 200,300 ' + __dirname + "/public/" + filename + extension;
-	shell.exec(cmd)
+	console.log("Processing with: fuzz(" + fuzz +"), saturation(" + saturation +"), brightness(" + brightness +")");
+	
+	takePic(file);
+	brightenPic(file, brightness, saturation);
+	
 	stopStreaming();
 	
 	res.json({img : filename});
@@ -93,7 +152,7 @@ app.get('/takePic', (req, res) => {
 // but this app won't be exposed outside the local environment.
 app.get('/save/:pic', (req, res) => {
 	console.log("Attempting to save temporary file.");
-	shell.mv(__dirname + "/public/" + req.params.pic, __dirname + '/pics/');
+	shell.mv(__dirname + "/public/" + req.params.pic + ".jpg", __dirname + '/pics/');
 	mkdir('./pics/' + req.params.pic).then(function(contents) {
 		res.json();
 	});
@@ -107,12 +166,12 @@ app.get('/delete/:pic', (req, res) => {
 });
 
 app.post('/process/:pic/:background', (req, res) => {
-	var output = processFile(req.params.pic, req.params.background);
+	var output = processFile(req.params.pic, req.params.background, process.env['photo_fuzz']);
 	res.json({output: output});
 })
 
 // This is where the magic happens. 
-function processFile(file, backgroundImage) {
+function processFile(file, backgroundImage, fuzz) {
 	console.log("Attempting to process!");
 	var dir = "./pics/" + file;
 	var originalPic = dir + ".jpg";
@@ -120,9 +179,7 @@ function processFile(file, backgroundImage) {
 	var rgbnum = "#44ff15"
 	var output = dir + "/output.png";
 	
-	// convert the image and remove the background
-	var cmd = 'convert ' + originalPic + ' -channel r -separate +channel -fuzz 8% -fill black -opaque black -fill white +opaque black ' + output;
-	shell.exec(cmd);	
+	processMask(originalPic, output, fuzz);
 
 	var outputImage = uuid() + ".png";
 	var outputPath = dir + "/" + outputImage;
@@ -138,6 +195,7 @@ function processFile(file, backgroundImage) {
 
 app.listen(3000, function() {
 	console.log("Running!");
+	loadSettings();
 	startStreaming();
 });
 
@@ -152,6 +210,53 @@ function startStreaming() {
 
 function stopStreaming() {
 	// Kill the streamer to free up some processing power.
+	console.log("Stopping streamer...");
 	shell.exec("pkill raspistill")
 	shell.exec("pkill mjpg_streamer");
+}
+
+function brightenPic(file, brightness, saturation) {
+	// Process the image here
+	var cmd = 'mogrify -modulate ' + (brightness || 0 )+',' + (saturation || 0) + ' ' + __dirname + "/public/" + file;
+	shell.exec(cmd)
+}
+
+function processMask(pic, output, fuzz) {
+	console.log(fuzz);
+	// convert the image and remove the background
+	var cmd = 'convert ' + pic + ' -channel r -separate +channel -fuzz ' + fuzz +'% -fill black -opaque black -fill white +opaque black ' + output;
+	shell.exec(cmd);	
+}
+
+function takePic(file) {
+	console.log("Taking pic: " + file);
+	if (process.env.photo_debug) {
+		shell.cp('-R', './sample.jpg', './public/' + file);
+	} else {
+		var cmd = "wget http://localhost:8080/?action=snapshot -O " + __dirname + "/public/" + file;
+		shell.exec(cmd); // This can't be async.. otherwise the streaming stops and there's no image!
+	}
+}
+
+function saveSettings(fuzz, brightness, saturation) {
+	var settings = fuzz + "," + brightness + "," + saturation
+	shell.rm("./settings.txt");
+	fs.writeFile("./settings.txt", settings , function(err) {
+		if(err) {
+			return console.log(err);
+		}
+
+		console.log("Settings Saved!");
+	}); 
+}
+
+// This should probably be in a database, but It's going on a RPI and I don't want anything chewing up processing. 
+function loadSettings() {
+	fs.readFile("./settings.txt", 'utf8', function(err, data) {
+	 	console.log("Settings loaded!");
+		var settings = data.split(",");
+		global.fuzz = settings[0];
+		global.brightness = settings[1];
+		global.saturation = settings[2];
+	});
 }
